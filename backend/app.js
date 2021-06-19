cfcaca45b06780c90ed9d129d636b66fcad252b7
Ap274 <aldopiresaps@gmail.com>
2021-06-19 12:39:45 -0300

    feat: added first route, controller and render minimal json

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,23 @@
-export default () => {
-  console.log('Hello World with Nodemon and Sucrase');
-};
+import express from 'express';
+import homeRoutes from './src/routes/homeRouter';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // utilizado para manipular conteudos em requisições POST
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
