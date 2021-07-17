03d65a966ee116363460e2f73c196aabd9ab70f6
Ap274 <aldopiresaps@gmail.com>
2021-07-17 12:43:52 -0300

    fix: updated column name updated_at

--- backend/src/database/migrations/20210717134706-users.js
+++ backend/src/database/migrations/20210717134706-users.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.DATE,
         alloNull: false,
       },
-      update_at: {
+      updated_at: {
         type: Sequelize.DATE,
         alloNull: false,
       },
