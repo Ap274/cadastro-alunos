55b431028e7a7e58e410ec735c10a5970ccb5aa9
Ap274 <aldopiresaps@gmail.com>
2021-07-31 12:34:11 -0300

    feat: created CRUD for users

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import UserController from '../controller/UserController';
+import User from '../model/User';
 
 const router = new Router();
 router.post('/', UserController.create);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
