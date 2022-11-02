c7e25b444295cafabbbc7d27710468cdb5f20b53
Ap274 <aldopiresaps@gmail.com>
2022-11-02 16:16:38 -0300

    feat: student form validator

--- frontend/src/pages/Student/styled.js
+++ frontend/src/pages/Student/styled.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  input {
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    margin: 0px 0px 20px 0px;
+    padding: 0px 10px;
+  }
+`;
