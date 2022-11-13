08d1a864038f8f9d99916ab56ad5171f722af25f
Ap274 <aldopiresaps@gmail.com>
2022-11-13 11:29:33 -0300

    feat: show student photo into student edit page

--- frontend/src/pages/Student/index.jsx
+++ frontend/src/pages/Student/index.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { isEmail, isInt, isFloat } from 'validator';
 import { get } from 'lodash';
 import { useDispatch } from 'react-redux';
+import { FaEdit, FaUserCircle } from 'react-icons/fa';
 import { Container } from '../../styles/GlobalStyles';
-import { Form } from './styled';
+import { Form, ProfilePicture, Title } from './styled';
 import Loading from '../../components/Loading';
 import axios from '../../services/axios';
 import * as actions from '../../store/modules/login/actions';
@@ -19,6 +20,7 @@ export default function Student() {
   const [height, setHeight] = useState('');
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState('');
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -31,6 +33,10 @@ export default function Student() {
 
         // buscar dados do aluno no backend
         const { data } = await axios.get(`/students/${id}`);
+
+        // buscar foto
+        const photoUrl = get(data, 'Photos[0].url', '');
+        setPhoto(photoUrl);
 
         // preencher os dados do aluno no formulário
         setName(data.name);
@@ -157,7 +163,22 @@ export default function Student() {
   return (
     <Container>
       <Loading isLoading={isLoading} />
-      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+      <Title>{id ? 'Editar aluno' : 'Novo aluno'}</Title>
+
+      {id && (
+        <ProfilePicture>
+          {photo ? (
+            <img src={photo} alt={name} />
+          ) : (
+            <FaUserCircle size={180} color="#000" />
+          )}
+
+          <Link to={`/photos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
+
       <Form onSubmit={handleSubmit}>
         <input
           type="text"
--- frontend/src/pages/Student/styled.js
+++ frontend/src/pages/Student/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as colors from '../../styles/colors';
 
 export const Form = styled.form`
   display: flex;
@@ -11,5 +12,36 @@ export const Form = styled.form`
     border: 1px solid #ddd;
     margin: 0px 0px 20px 0px;
     padding: 0px 10px;
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const ProfilePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+  position: relative;
+
+  img {
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+  }
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 8px;
+    color: white;
+    background-color: ${colors.primaryColor};
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
   }
 `;
